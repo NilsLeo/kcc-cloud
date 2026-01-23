7da1598b396c3ca85096059ff665c6c1da55aec2
NilsLeo <nilstleo01@gmail.com>
2026-01-23 02:13:57 +0100

    got rid of nginx

--- infra/entrypoint.sh
+++ infra/entrypoint.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 set -e
 
-# Calculate upload size in MB from bytes (default 1GB = 1073741824 bytes)
-MAX_UPLOAD_SIZE=${MAX_UPLOAD_SIZE:-1073741824}
-export MAX_UPLOAD_SIZE_MB=$((MAX_UPLOAD_SIZE / 1024 / 1024))
-
-echo "Configuring nginx with MAX_UPLOAD_SIZE_MB=${MAX_UPLOAD_SIZE_MB}M"
-
-# Process nginx config template
-envsubst '${MAX_UPLOAD_SIZE_MB}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-# Test nginx config
-nginx -t
-
 # Ensure application data directory exists and is writable by appuser
 mkdir -p /data /data/tmp
 chown -R appuser:appuser /data || true
 
-# Start supervisord
+# Start supervisord (backend, celery, next, redis)
 exec /usr/bin/supervisord -n
